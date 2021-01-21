435bde1fc0e79e0afc2ae139951ac6d839656fa7
Rebecca Chiquete <deerinra194@gmail.com>
2021-01-20 21:19:50 -0700

    updatedtable

--- index.js
+++ index.js
@@ -69,13 +69,13 @@ inquirer
 ${data.title}
 
 ## Table of Contents 
-    1. [Project Description] (#Project-Description)
-    1. [Data Installation] (#Data-Installation)
-    1. [Data Usage] (#Data-Usage)
-    1. [Contributions] (#Contributions)
-    1. [Instructions] (#Instructions)
-    1. [Questions] (#Questions)
-    1. [License] (#License)
+1. [Project Description](#Project-Description)
+1. [Data Installation](#Data-Installation)
+1. [Data Usage](#Data-Usage)
+1. [Contributions](#Contributions)
+1. [Instructions](#Instructions)
+1. [Questions](#Questions)
+1. [License](#License)
     
 
 ###  Project Description 
--- title.md
+++ title.md
@@ -4,13 +4,13 @@
 Title
 
 ## Table of Contents 
-    1. [Project Description] (#Project-Description)
-    1. [Data Installation] (#Data-Installation)
-    1. [Data Usage] (#Data-Usage)
-    1. [Contributions] (#Contributions)
-    1. [Instructions] (#Instructions)
-    1. [Questions] (#Questions)
-    1. [License] (#License)
+1. [Project Description](#Project-Description)
+1. [Data Installation](#Data-Installation)
+1. [Data Usage](#Data-Usage)
+1. [Contributions](#Contributions)
+1. [Instructions](#Instructions)
+1. [Questions](#Questions)
+1. [License](#License)
     
 
 ###  Project Description 
